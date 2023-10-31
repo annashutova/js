5d3590ab50e80482517ac58b9eff3e80834f6516
annashutova <annashut@inbox.ru>
2023-10-31 20:04:35 +0300

    refactor: use Calculator class for storing logic

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,68 +1,82 @@
-let display = document.getElementById("display")
-let equation = ""
-const historyList = document.getElementById("history-list")
-const historyContent = document.querySelector(".history-content")
-let history = []
-const symbols = ['+', '*', '/', '.']
-
-function toggleHistory() {
-    if (historyContent.style.display === "block") {
-      historyContent.style.display = "none"
-    } else {
-      updateHistory()
-      historyContent.style.display = "block"
+class Calculator {
+    constructor() {
+        this.display = document.getElementById("display"),
+        this.equation = '',
+        this.symbols = ['+', '*', '/', '.'],
+        this.history = [],
+        this.historyList = document.getElementById("history-list"),
+        this.historyContent = document.querySelector(".history-content")
     }
-}
-  
-function updateHistory() {
-    historyList.innerHTML = ""
-    history.forEach((entry) => {
-        const listItem = document.createElement("li")
-        listItem.textContent = entry
-        listItem.addEventListener("click", () => {
-            display.value = entry
-            equation = entry
+
+    appendToDisplay(value) {
+        const lastSymbol = this.equation.slice(-1)
+        if (this.symbols.includes(value)) {
+            if (this.equation.length === 0) {
+                return
+            } else if (this.symbols.includes(lastSymbol)) return
+        }
+        if (lastSymbol === '-' && (value === '-' || this.symbols.includes(value))) return
+        this.equation += value
+        this.display.value = this.equation
+    }
+
+    clearDisplay() {
+        this.equation = ""
+        this.display.value = ""
+    }
+
+    removeLastSymbol() {
+        this.equation = this.equation.slice(0, -1)
+        this.display.value = this.equation
+    }
+
+    calculateResult() {
+        try {
+            this.equation = eval(this.equation).toString()
+            this.display.value = this.equation
+            this.addToHistory(this.equation)
+        } catch (error) {
+            console.log(error)
+            this.display.value = "Error"
+        }
+    }
+
+    addToHistory(entry) {
+        this.history.unshift(entry)
+        if (this.history.length > 10) {
+            this.history.pop()
+        }
+    }
+
+    updateHistory() {
+        this.historyList.innerHTML = ""
+        this.history.forEach((entry) => {
+            const listItem = document.createElement("li")
+            listItem.textContent = entry
+            listItem.addEventListener("click", () => {
+                this.display.value = entry
+                this.equation = entry
+            })
+            listItem.style.cursor = 'pointer'
+            this.historyList.appendChild(listItem)
         })
-        listItem.style.cursor = 'pointer'
-        historyList.appendChild(listItem)
-    })
-}
+    }
 
-function addToHistory(entry) {
-    history.unshift(entry)
-    if (history.length > 10) {
-        history.pop()
+    toggleHistory() {
+        if (this.historyContent.style.display === "block") {
+          this.historyContent.style.display = "none"
+        } else {
+          this.updateHistory()
+          this.historyContent.style.display = "block"
+        }
     }
 }
 
-function appendToDisplay(value) {
-    const lastSymbol = equation.slice(-1)
-    if (symbols.includes(value)) {
-        if (equation.length === 0) {
-            return
-        } else if (symbols.includes(lastSymbol)) return
-    }
-    if (lastSymbol === '-' && (value === '-' || symbols.includes(value))) return
-    equation += value
-    display.value = equation
-}
+const calculator = new Calculator()
+const history = document.getElementById("history-button")
+const clearAll = document.getElementById("clear-button")
+const evaluate = document.getElementById("calculate-button")
 
-function clearDisplay() {
-    equation = ""
-    display.value = ""
-}
-
-function removeLastSymbol() {
-    equation = equation.slice(0, -1)
-    display.value = equation
-}
-
-function calculateResult() {
-    try {
-        equation = eval(equation).toString()
-        display.value = equation
-        addToHistory(equation)
-    } catch (error) {
-        display.value = "Error"
-    }
-}
+history.addEventListener('click', () => calculator.toggleHistory())
+clearAll.addEventListener('click', () => calculator.clearDisplay())
+evaluate.addEventListener('click', () => calculator.calculateResult())
