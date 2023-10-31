ef32966aaa5bbd40e10200300f10b14b46f2931b
annashutova <annashut@inbox.ru>
2023-10-31 19:09:49 +0300

    added: js code for dropdown logic

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,6 +1,39 @@
 let display = document.getElementById("display")
 let equation = ""
+const historyList = document.getElementById("history-list")
+const historyContent = document.querySelector(".history-content")
+let history = []
 const symbols = ['+', '*', '/', '.']
+
+function toggleHistory() {
+    if (historyContent.style.display === "block") {
+      historyContent.style.display = "none"
+    } else {
+      updateHistory()
+      historyContent.style.display = "block"
+    }
+}
+  
+function updateHistory() {
+    historyList.innerHTML = ""
+    history.forEach((entry) => {
+        const listItem = document.createElement("li")
+        listItem.textContent = entry
+        listItem.addEventListener("click", () => {
+            display.value = entry
+            equation = entry
+        })
+        listItem.style.cursor = 'pointer'
+        historyList.appendChild(listItem)
+    })
+}
+
+function addToHistory(entry) {
+    history.unshift(entry)
+    if (history.length > 10) {
+        history.pop()
+    }
+}
 
 function appendToDisplay(value) {
     const lastSymbol = equation.slice(-1)
@@ -28,6 +61,7 @@ function calculateResult() {
     try {
         equation = eval(equation).toString()
         display.value = equation
+        addToHistory(equation)
     } catch (error) {
         display.value = "Error"
     }
